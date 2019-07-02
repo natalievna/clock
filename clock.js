9c9340d2eae6faa4a79e51be9d537db344ba4edd
natalievna <natalievna11@gmail.com>
2019-07-02 20:50:28 +0300

    clock

---
+++ clock.js
@@ -0,0 +1,22 @@
+setInterval(() => {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let period = "AM";
+
+    if (hours >= 12) {
+        period = "PM";
+    }
+    if (hours > 12) {
+        hours = hours - 12;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let clockTime = `${hours}:${minutes}:${seconds} ${period}`;
+
+    let clock = document.getElementById('clock');
+    clock.innerText = clockTime;
+},1000)
